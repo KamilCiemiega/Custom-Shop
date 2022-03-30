e0ec1b54c06bf05b9f44e6d076e42fd75a32be3c
unknown <kamil13241012@interia.pl>
2022-03-30 08:11:29 -0700

    changes

--- src/components/BestSell.tsx
+++ src/components/BestSell.tsx
@@ -2,16 +2,20 @@ import React,{ useState } from 'react';
 import classes from './BestSell.module.css';
 import { AiOutlineHeart } from "react-icons/ai";
 import { MdOutlinePageview } from "react-icons/md"
-
+import { AiOutlineClose } from "react-icons/ai"
 
 const heartStyle = {width:'20px', height:'20px'};
 const viewStyle = {width:'20px', height:'20px'};
+const closeStyle = {position:'absolute', top:'0', right:'0'}
 
 const BestSell = () => {
 
-    const [showDress,setShowDress] = useState<boolean>(false)
-    const [showtshirt,setShowtshirt] = useState<boolean>(false)
-    const [showFit,setShowFit] = useState<boolean>(false)
+    const [showDress,setShowDress] = useState<boolean>(false);
+    const [showtshirt,setShowtshirt] = useState<boolean>(false);
+    const [showFit,setShowFit] = useState<boolean>(false);
+    const [showModalDress, setShowModalDress] = useState<boolean>(false);
+    const [showModaltshirt, setShowModaltshirt] = useState<boolean>(false);
+    const [showModalFit, setShowModalFit] = useState<boolean>(false);
 
     return(
         <div className={classes.wrapper}>
@@ -38,15 +42,35 @@ const BestSell = () => {
                         <AiOutlineHeart style={heartStyle}/>
                     </button>
                         <button className={classes.viewButton}>
-                        <MdOutlinePageview style={viewStyle}/>
+                        <MdOutlinePageview style={viewStyle} onClick={() => setShowModalDress(true)}/>
                     </button>
                     </div>
                     <button className={classes.addToCard}>Add To Card</button>
                     </div>
                     }
+                    {showModalDress &&
+                    <div className={classes.modal}>
+                    <div className={classes.modalElement}>
+                        <img src='https://andshop-vue.netlify.app/_nuxt/img/7.6533a8a.png'></img>
+                        <div className={classes.modalText}>
+                            <h1>Trendy woman black blazer</h1>
+                            <div className={classes.modalTextPrice}>
+                            <p>$98</p>
+                            <s>$120</s>
+                            </div>
+                            <p>Santiago who travels from his homeland in Spain to the Egyptian desert in search of a treasure buried near the Pyramids. Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur adipiscing elitet.</p>
+                            <p>Vendor: Lereve</p>
+                            <p>Categories: Hoodie</p>
+                            <button>Add to Card</button>
+                            <AiOutlineClose className={classes.closeIcon} onClick={() => setShowModalDress(false)}/>
+                        </div>
+                    </div>
+                </div>
+                    }
+                    
                 </div>
 
-            <div 
+                <div 
                 className={classes.wrapperElementsItem}
                 onMouseEnter={() => setShowtshirt(true)}
                 onMouseLeave={() => setShowtshirt(false)}>
@@ -63,11 +87,30 @@ const BestSell = () => {
                         <AiOutlineHeart style={heartStyle}/>
                     </button>
                         <button className={classes.viewButton}>
-                        <MdOutlinePageview style={viewStyle}/>
+                        <MdOutlinePageview style={viewStyle} onClick={() => setShowModaltshirt(true)}/>
                     </button>
                     </div>
                     <button className={classes.addToCard}>Add To Card</button>
                     </div>
+                    }
+                    {showModaltshirt &&
+                    <div className={classes.modal}>
+                    <div className={classes.modalElement}>
+                        <img src='https://andshop-vue.netlify.app/_nuxt/img/1.7190443.png'></img>
+                        <div className={classes.modalText}>
+                            <h1>Trendy woman black blazer</h1>
+                            <div className={classes.modalTextPrice}>
+                            <p>$48</p>
+                            <s>$120</s>
+                            </div>
+                            <p>Santiago who travels from his homeland in Spain to the Egyptian desert in search of a treasure buried near the Pyramids. Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur adipiscing elitet.</p>
+                            <p>Vendor: Lereve</p>
+                            <p>Categories: Hoodie</p>
+                            <button>Add to Card</button>
+                            <AiOutlineClose className={classes.closeIcon} onClick={() => setShowModaltshirt(false)}/>
+                        </div>
+                    </div>
+                </div>
                     }
                 </div>
 
@@ -85,15 +128,33 @@ const BestSell = () => {
                         <AiOutlineHeart style={heartStyle}/>
                     </button>
                         <button className={classes.viewButton}>
-                        <MdOutlinePageview style={viewStyle}/>
+                        <MdOutlinePageview style={viewStyle} onClick={() => setShowModalFit(true)}/>
                     </button>
                     </div>
                     <button className={classes.addToCard}>Add To Card</button>
                     </div>
                     }
+                    {showModalFit &&
+                    <div className={classes.modal}>
+                    <div className={classes.modalElement}>
+                        <img src='https://andshop-vue.netlify.app/_nuxt/img/11.54a1591.png'></img>
+                        <div className={classes.modalText}>
+                            <h1>Trendy woman black blazer</h1>
+                            <div className={classes.modalTextPrice}>
+                            <p>$64</p>
+                            <s>$120</s>
+                            </div>
+                            <p>Santiago who travels from his homeland in Spain to the Egyptian desert in search of a treasure buried near the Pyramids. Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor incididunt ut labore et. Lorem ipsum dolor sit amet, consectetur adipiscing elitet.</p>
+                            <p>Vendor: Lereve</p>
+                            <p>Categories: Hoodie</p>
+                            <button>Add to Card</button>
+                            <AiOutlineClose className={classes.closeIcon} onClick={() => setShowModalFit(false)}/>
+                        </div>
+                    </div>
+                </div>
+                    }
                 </div>
             </div>
-            {/* https://preview.themeforest.net/item/andshop-ecommerce-vue-js-template/full_screen_preview/32523571?_ga=2.172952483.106436678.1648617215-484990084.1648113748 */}
         </div>
     );
 };
